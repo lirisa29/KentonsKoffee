a5a05d3d39f348d0146896e830541e599d88cd22
lirisa29 <lirisagovender@gmail.com>
2025-09-09 12:22:04 +0200

    Set up RoomDB

--- app/src/main/java/com/iie/st10349354/kentonskoffee/MainActivity.kt
+++ app/src/main/java/com/iie/st10349354/kentonskoffee/MainActivity.kt
@@ -7,16 +7,24 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.navigation.NavigationView
 import com.iie.st10349354.kentonskoffee.databinding.ActivityMainWithNavDrawerBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     var order = Order()
+
     private lateinit var binding: ActivityMainWithNavDrawerBinding
     private lateinit var toggleOnOff: ActionBarDrawerToggle
+    private lateinit var orderDAO : OrderDAO // Declare DAO
+    private lateinit var db: AppDatabase // Declare Database
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +32,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, NavigationView.O
 
         binding = ActivityMainWithNavDrawerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialise Database and DAO
+        db = AppDatabase.getDatabase(this) as AppDatabase
+        orderDAO = db.orderDAO()
 
         // Window insets for edge-to-edge layout
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -54,16 +66,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, NavigationView.O
         binding.imgSb1.setOnClickListener(this)
         binding.imgSb2.setOnClickListener(this)
         binding.imgSb3.setOnClickListener(this)
+
+        insertProducts()
+    }
+
+    private fun insertProducts() {
+        val products = listOf(
+            OrderEntity(productName = "Soy Latte", customerName = "", customerCell = "", orderDate = ""),
+            OrderEntity(productName = "Chocco Frappe", customerName = "", customerCell = "", orderDate = ""),
+            OrderEntity(productName = "Caramel Frappe", customerName = "", customerCell = "", orderDate = "")
+        )
+
+        CoroutineScope(Dispatchers.IO).launch {
+            products.forEach { orderDAO.insert(it)}
+        }
     }
 
     override fun onClick(v: View?){
         when(v?.id){
-            R.id.img_sb1 -> order.productName = "Soy Latte"
-            R.id.img_sb2 -> order.productName = "Chocco Frappe"
-            R.id.img_sb3 -> order.productName = "Caramel Frappe"
+            R.id.img_sb1 -> getProductFromDB("Soy Latte")
+            R.id.img_sb2 -> getProductFromDB("Chocco Frappe")
+            R.id.img_sb3 -> getProductFromDB("Caramel Frappe")
         }
         Toast.makeText(this@MainActivity, "MMM " + order.productName, Toast.LENGTH_SHORT).show()
         openIntent(applicationContext, order.productName, OrderDetailsActivity::class.java)
+    }
+
+    private fun getProductFromDB(productName: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val product = orderDAO.getAllOrders().firstOrNull { it.productName == productName}
+            product?.let {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity,
+                        "MMM ${it.productName}", Toast.LENGTH_SHORT).show()
+                    openIntent(applicationContext, it.productName, OrderDetailsActivity::class.java)
+                }
+            }
+        }
     }
 
     override fun onBackPressed(){
---
+++ app/src/main/java/com/iie/st10349354/kentonskoffee/OrderDAO.kt
@@ -0,0 +1,14 @@
+package com.iie.st10349354.kentonskoffee
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface OrderDAO {
+    @Insert
+    fun insert(orderEntity: OrderEntity): Long
+
+    @Query("SELECT * FROM orders")
+    fun getAllOrders(): List<OrderEntity>
+}
